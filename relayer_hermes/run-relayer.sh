5bcd79f02ac610bd586609d0989ffd9d048f47f1
Leonard Lepadatu <leonardlepadatu@yahoo.com>
2023-04-28 21:53:24 +0300

    update hermes to 1.4.0 and update the config with pulsar-2 testnet

--- relayer_hermes/run-relayer.sh
+++ relayer_hermes/run-relayer.sh
@@ -3,5 +3,6 @@
 hermes keys add --chain secretdev-1 --mnemonic-file "secret.json" --hd-path "m/44'/529'/0'/0/0"
 hermes keys add --chain localosmosis --mnemonic-file "osmosis.json"
 
+hermes create channel --a-chain secretdev --b-chain localosmosis --a-port transfer --b-port transfer --new-client-connection
 hermes create channel --a-chain localosmosis --b-chain secretdev-1 --a-port transfer --b-port transfer --new-client-connection
 hermes start
